f7a6de303c4bac9119963f1336c7dbcd7b607e4c
useStore <1409670346@qq.com>
2021-08-11 17:54:53 +0800

    '测试'

--- routes/index.js
+++ routes/index.js
@@ -4,45 +4,45 @@ var axios = require('axios');
 var getInitOrders = require('../json/getInitOrders.json');
 var baseURL = 'http://yapi.smart-xwork.cn';
 // var getInitOrders = require('../json/getInitOrders.json');
-var resdata =async (url)=>{
-  var data =await axios.get(`${baseURL}/mock/82363/api${url}`).catch(err=>{
+var resdata = async (url) => {
+  var data = await axios.get(`${baseURL}/mock/82363/api${url}`).catch(err => {
     console.log(err);
     return err;
   });
   return data;
 }
 /* GET home page. */
-router.get('/',function(req,res){
-res.send(getInitOrders);
+router.get('/', function (req, res) {
+  res.send(getInitOrders);
 });
-router.route('/getInitOrders').get((req,res)=>{
-  new Promise((resolve,reject)=>{
+router.route('/getInitOrders').get((req, res) => {
+  new Promise((resolve, reject) => {
     resolve(resdata('/getInitOrders'));
-  }).then((res)=>{
+  }).then((res) => {
     var data = res.data;
     return data;
-  }).then((json)=>{
-
-  res.send(json);
+  }).then((json) => {
+    json.wh = '测试';
+    res.send(json);
   })
 })
-router.route('/getCompleteOrders').get((req,res)=>{
-  const {orders,counts} = req.query;
-  if(!orders || !counts){
+router.route('/getCompleteOrders').get((req, res) => {
+  const { orders, counts } = req.query;
+  if (!orders || !counts) {
     res.send({
-      "code":110,
-      "data":"失败"
+      "code": 110,
+      "data": "失败"
     })
   }
-  new Promise((resolve,reject)=>{
+  new Promise((resolve, reject) => {
     resolve(resdata(`/getCompleteOrders?counts=${counts}&orders=${orders}`));
-  }).then((res)=>{
+  }).then((res) => {
     var data = res.data;
     return data;
-  }).then((json)=>{
+  }).then((json) => {
 
-  res.send(json);
-  }).catch(err=>{
+    res.send(json);
+  }).catch(err => {
 
   })
 })
